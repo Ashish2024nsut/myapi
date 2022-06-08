a4d83d1c88cceaca64d4fd086a30a375f702adf9
Ashish2024nsut <ashishphog.ug20@gmail.com>
2022-06-09 01:06:08 +0530

    getspecific user

--- routers/user.js
+++ routers/user.js
@@ -4,14 +4,41 @@ const userModel = require('../models/user');
 //creating router
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.json({"message":"you are on the user page"});
+router.get('/',async (req,res)=>{
+    const users = await userModel.find();
+    res.json({"message":"the users registered are: ",
+users : users});
 })
 
-router.post('/register',(req,res)=>{
+/*
+route               '/:id'
+desc                getting specific user by id
+access              admin
+pramas              id
+method              get
+*/
+
+router.get('/:id', async(req,res)=>{
+    const getspecificuser = await userModel.find({rollNo : parseInt(req.params.id)});
+    if(!getspecificuser){
+        res.json({"message": `no user exists with id : ${req.params.id}`});
+    }
+    console.log(getspecificuser);
+    res.json({"message" : "user found",
+    "user" : getspecificuser});
+});
+
+router.post('/register', async (req,res)=>{
     const user = new userModel(req.body);
 
-    user.save().then(()=>{
+    const checkuser = await userModel.find({rollNo : req.body.rollNo});
+
+    if(checkuser.length != 0){
+        console.log(checkuser);
+        res.json({"message": "user already exists"});
+        return ;
+    }
+    await user.save().then(()=>{
         res.send({"message":"user created succesfully",
     "user": user});
     }).catch(err=>{
